9e5968f0b9556edf00f38009340706611a4e03df
root <root@dev2.pbiaas.com>
2014-06-02 16:08:20 +0200

    altes Slider-Markup entfernt (war auskommentiert)

--- templates/regiomino_featureslider_theme_featureslider.tpl.php
+++ templates/regiomino_featureslider_theme_featureslider.tpl.php
@@ -3,19 +3,10 @@
         <div class="featured">
             <div class="box-head clearfix">
                 <h2><?php print t("Neueste Produkte"); ?></h2>
-                    <!--<div class="pagers center">
-                            <a class="prev featuredPrev" href="#prev"> </a>
-                            <a class="nxt featuredNxt" href="#nxt"> </a>
-                    </div>-->
-            </div><!--end box_head -->
+                    
+            </div>
     
-            <div> <!--class="cycle-slideshow clearfix" 
-            data-cycle-fx="scrollHorz"
-            data-cycle-timeout=0
-            data-cycle-slides="> ul"
-            data-cycle-prev="div.pagers a.featuredPrev"
-            data-cycle-next="div.pagers a.featuredNxt"
-            -->
+            <div> 
             <?php
             $count = 0;
             $length = count($vars["products"]);
@@ -33,7 +24,7 @@
                     }
             ?>
                 <li class="clearfix column">
-                    <div class="img">
+                    <div class="img preloaded">
                         <?php if ($reduziert): ?>
                             <div class="ribbon-green"></div>
                         <?php endif; ?>
@@ -48,8 +39,11 @@
                             </div>
                         
                         <a class="image" href="<?php echo $product['path'];?>">
-                                <!--<img src="<?php echo image_style_url('productgrid_sixteen', $product['image']);?>" alt="<?php echo $product['title'];?>">-->
-				<?php echo $product['image_full']; ?>
+                            <img class="preloader" src="<?php echo base_path() . path_to_theme();?>/images/preloaders/preloader.gif" data-src="<?php echo $product['img_path'];?>"">
+                        
+                            <noscript> 
+                            <?php echo $product['image_full']; ?>
+                            </noscript>
                         </a> 
                     </div>
                     <h6>
