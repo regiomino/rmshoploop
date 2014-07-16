a4ea33445644dd84247ebe4dcf004909eb146740
root <root@ip78-137-98-197.pbiaas.com>
2014-07-16 15:49:39 +0200

    GA Tracking Code manuell statt ueber Modul eingefuegt

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -15,6 +15,18 @@
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-40144139-1', 'auto');
+			ga('send', 'pageview');
+
+		</script>
+
     <div id="skip-link">
       <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
     </div>
@@ -27,6 +39,20 @@
              $(".rmnav").rmNav();
 
         })(jQuery);
+				(function () { 
+						var _tsid = 'X9FF4FD2592B5BFA727E6480148224623'; 
+						_tsConfig = { 
+								'yOffset': '0', //offset from page bottom
+								'variant': 'reviews' //text, default, small, reviews
+						};
+						var _ts = document.createElement('script');
+						_ts.type = 'text/javascript'; 
+						_ts.charset = 'utf-8'; 
+						_ts.src = '//widgets.trustedshops.com/js/' + _tsid + '.js'; 
+						var __ts = document.getElementsByTagName('script')[0];
+						__ts.parentNode.insertBefore(_ts, __ts);
+				})();
     </script>
+
 </body>
 </html>
